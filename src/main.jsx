7ba2fad77998ca2d9f7f9c23269ddb5ad20fab13
ShamimHossain1 <beastb697@gmail.com>
2023-04-03 17:55:40 +0600

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,18 +6,54 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import ErrorPage from './components/Errorpage/Error';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App></App>,
+//   },
+//   {
+//     path:'/about',
+//     element: <About></About>,
+//   },
+//   {
+//     path:'/contact',
+//     element: <Contact></Contact>,
+//   },
+//   {
+//     path: "/",
+//     errorElement:<ErrorPage></ErrorPage> ,
+//   },
+// ])
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App></App>,
-  },
-]);
+    path:'/',
+    element: <Home></Home> ,
+    children:[
+      {
+        path:'about',
+        element:<About></About>,
+
+      },
+      {
+        path:'contact',
+        element:<Contact></Contact>,
+      },
+    ]
+  }
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <Header></Header> */}
      <RouterProvider router={router} />
   </React.StrictMode>,
 )
